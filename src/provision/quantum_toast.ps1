2bcd99f962641a873b8e191855f9898040048ea7
Anton <vanasscheanton@gmail.com>
2023-10-26 11:15:41 +0200

    Add QuantumToast deploy script

---
+++ src/provision/quantum_toast.ps1
@@ -0,0 +1,15 @@
+Install-WindowsFeature –ConfigurationFilePath Z:\files\quantum_toast_role_install_config.xml
+
+Import-Module ADDSDeployment
+Install-ADDSForest `
+    -CreateDnsDelegation:$false `
+    -DatabasePath "C:\Windows\NTDS" `
+    -DomainMode "WinThreshold" `
+    -DomainName "WS2-2324-anton.hogent" `
+    -DomainNetbiosName "WS2-2324-ANTON" `
+    -ForestMode "WinThreshold" `
+    -InstallDns:$true `
+    -LogPath "C:\Windows\NTDS" `
+    -NoRebootOnCompletion:$false `
+    -SysvolPath "C:\Windows\SYSVOL" `
+    -Force:$true
