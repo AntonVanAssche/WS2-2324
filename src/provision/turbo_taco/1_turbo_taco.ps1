91a63ba37d5b4250cd8d938265128b0b2855a72c
Anton <vanasscheanton@gmail.com>
2023-11-08 20:56:16 +0100

    Add initial TurboTaco ps1 script

---
+++ src/provision/turbo_taco/1_turbo_taco.ps1
@@ -0,0 +1,13 @@
+﻿$interface_config = @{
+    InterfaceAlias = "Ethernet"
+    IPAddress = "192.168.23.2"
+    PrefixLength = "24"
+}
+$pass = "Friday13th!"
+
+try{
+    New-NetIPAddress @interface_config
+
+} catch {
+    Write-Host $("(Configuring network settings failed: "+ $_.Exception.Message)
+}
