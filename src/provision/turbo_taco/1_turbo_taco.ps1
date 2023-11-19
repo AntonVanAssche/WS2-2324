9aa3d72a9e098fbabd8014ba4b830b2311eb1c9c
Anton <vanasscheanton@gmail.com>
2023-11-19 15:54:57 +0100

    Make TurboTaco join the domain

--- src/provision/turbo_taco/1_turbo_taco.ps1
+++ src/provision/turbo_taco/1_turbo_taco.ps1
@@ -3,11 +3,32 @@
     IPAddress = "192.168.23.3"
     PrefixLength = "24"
 }
+
+$dns_config = @{
+    InterfaceAlias = "Ethernet"
+    ServerAddresses = @("192.168.23.2")
+}
+
 $pass = "Friday13th!"
 
 try{
     New-NetIPAddress @interface_config
-
 } catch {
-    Write-Host $("(Configuring network settings failed: "+ $_.Exception.Message)
+    Write-Error $("(Failed to configuring network settings: "+ $_.Exception.Message)
+}
+
+try{
+    Set-DnsClientServerAddress @dns_config
+} catch {
+    Write-Error $("(Failed to configuring DNS settings: "+ $_.Exception.Message)
+}
+
+try{
+    Add-Computer -ComputerName TurboTaco `
+        -LocalCredential taco\Administrator `
+        -DomainName WS2-2324-anton.hogent `
+        -Credential WS2-2324-anton.hogent\Administrator `
+        -Force
+} catch {
+    Write-Error $("(Failed to join domain: "+ $_.Exception.Message)
 }
