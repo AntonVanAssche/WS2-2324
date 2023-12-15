4810eacbc6417c3ec628c4f2013c5151fe2d4553
Anton <vanasscheanton@gmail.com>
2023-12-15 15:27:41 +0100

    Split TurboTaco provisioning scripts into three scripts

---
+++ src/provision/turbo_taco/2_turbo_taco.ps1
@@ -0,0 +1,77 @@
+﻿
+try {
+    $mount_path = "F:\"
+    Start-Process -FilePath "$mount_path\setup.cmd" `
+        -ArgumentList "/config $config_path" `
+        -Wait
+} catch {
+    Write-Error $("(Failed to install SharePoint Server: "+ $_.Exception.Message)
+}
+
+try {
+    Add-PSSnapin Microsoft.SharePoint.PowerShell
+    $pass = "Friday13th!"
+    $farm_config = @{
+        DatabaseServer = "TofuTerminator"
+        DatabaseName = "MSSQLSERVER"
+        Passphrase = (ConvertTo-SecureString $pass -AsPlainText -force)
+        AdministrationContentDatabaseName = "SharePoint_AdminContent"
+        FarmCredentials = (Get-Credential "WS2-2324-anton\Administrator")
+        LocalServerRole = "SingleServerFarm"
+    }
+
+    New-SPConfigurationDatabase @farm_config
+} catch {
+    Write-Error $("(Failed to create SharePoint Farm: "+ $_.Exception.Message)
+}
+
+try {
+    Initialize-SPResourceSecurity
+} catch {
+    Write-Error $("(Failed to install SharePoint Resources: "+ $_.Exception.Message)
+}
+
+try {
+    Install-SPService
+} catch {
+    Write-Error $("(Failed to install SharePoint Services: "+ $_.Exception.Message)
+}
+
+try {
+    Install-SPFeature -AllExistingFeatures
+} catch {
+    Write-Error $("(Failed to install SharePoint Features: "+ $_.Exception.Message)
+}
+
+try {
+    New-SPCentralAdministration -Port 2016 -WindowsAuthProvider NTLM
+} catch {
+    Write-Error $("(Failed to create Central Administration: "+ $_.Exception.Message)
+}
+
+try {
+    Install-SPHelpCollection -All
+} catch {
+    Write-Error $("(Failed to install help: "+ $_.Exception.Message)
+}
+
+try {
+    Install-SPApplicationContent
+} catch {
+    Write-Error $("(Failed to application content: "+ $_.Exception.Message)
+}
+
+try {
+    New-NetFirewallRule -DisplayName "ICMPv4 Allow Ping" `
+        -Direction Inbound `
+        -Protocol ICMPv4 `
+        -IcmpType 8 `
+        -Action Allow
+    New-NetFirewallRule -DisplayName "ICMPv6 Allow Ping" `
+        -Direction Inbound `
+        -Protocol ICMPv6 `
+        -IcmpType 8 `
+        -Action Allow
+} catch {
+    Write-Error $("(Failed to configure firewall: "+ $_.Exception.Message)
+}
---
+++ src/provision/turbo_taco/3_turbo_taco.ps1
@@ -0,0 +1,29 @@
+﻿try {
+    $AuthProvider = New-SPAuthenticationProvider `
+        -UseWindowsIntegratedAuthentication `
+        -DisableKerberos
+
+    New-SPWebApplication `
+        -Name "IntranetSite" `
+        -Port 8080 `
+        -HostHeader "intranet.WS2-2324-anton.hogent" `
+        -URL "http://intranet.ws2-2324-anton.hogent/" `
+        -ApplicationPool "IntranetAppPool" `
+        -ApplicationPoolAccount (Get-SPManagedAccount "WS2-2324-ANTON\Administrator") `
+        -DatabaseName "SP_ContentDB" `
+        -DatabaseServer "TofuTerminator.WS2-2324-ANTON.hogent" `
+        -AuthenticationProvider $AuthProvider `
+        -Verbose
+} catch {
+    Write-Error $("(Failed to create web application: "+ $_.Exception.Message)
+}
+
+try {
+    New-SPSite `
+        -Url "http://intranet.ws2-2324-anton.hogent:8080/" `
+        -OwnerAlias "WS2-2324-anton\Administrator" `
+        -Template "STS#0" `
+        -Verbose
+} catch {
+    Write-Error $("(Failed to create site collection: "+ $_.Exception.Message)
+}
